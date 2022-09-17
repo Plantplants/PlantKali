63bc6ff21e2c2808a055094a49b116111609e6fb
Plant Plants <77038776+Plantplants@users.noreply.github.com>
2022-09-17 13:17:30 -0700

    Update plantkali.sh

--- plantkali.sh
+++ plantkali.sh
@@ -15,6 +15,9 @@ cd /home/plantplants
 mkdir toolbox
 cd toolbox
 
-# Background
+# Backgrounds/logos
 apt install -y kali-wallpapers-legacy
 xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitorVirtual1/workspace0/last-image --set /usr/share/backgrounds/kali-heart/kali-heart-wp-blue-1920x1080.jpg
+cp /usr/share/backgrounds/kali-heart/kali-heart-wp-1920x1080.jpg /usr/share/desktop-base/kali-theme/login/background
+curl 'https://upload.wikimedia.org/wikipedia/en/2/2d/SSU_Kirby_artwork.png' > /home/plantplants/Pictures/kirby.png
+sed -i 's/#emblem-kali/\/home\/plantplants\/Pictures\/kirby.png/' /etc/lightdm/lightdm-gtk-greeter.conf
