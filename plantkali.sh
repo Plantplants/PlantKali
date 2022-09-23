13e84e6b79f2c52545417ebec02b1b038356e1f9
Plant Plants <77038776+Plantplants@users.noreply.github.com>
2022-09-23 13:12:57 -0700

    Update plantkali.sh

--- plantkali.sh
+++ plantkali.sh
@@ -7,8 +7,8 @@ echo 'Run as root'
 userhome="/home/plantplants"
 toolbox="/home/plantplants/toolbox"
 
-# apt update
-# apt upgrade
+apt update
+apt upgrade
 
 # Setting up plantplants
 pkill -u kali pid ; pkill -9 -u kali
@@ -24,6 +24,10 @@ echo 'export PATH="$PATH:$toolbox"' >> /home/plantplants/.zshrc
 apt install -y kali-wallpapers-legacy
 curl 'https://upload.wikimedia.org/wikipedia/en/2/2d/SSU_Kirby_artwork.png' > /home/plantplants/Pictures/kirby.png
 xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVirtual1/workspace0/last-image --set /usr/share/backgrounds/kali-heart/kali-heart-wp-blue-1920x1080.jpg
+echo '#!/bin/bash\n xfconf-query -c xfce4-desktop
+  -p /backdrop/screen0/monitorVirtual1/workspace0/last-image
+  --set /usr/share/backgrounds/kali-heart/kali-heart-wp-blue-1920x1080.jpg'
+  > $toolbox/setup.sh
 cp /usr/share/backgrounds/kali-heart/kali-heart-wp-1920x1080.jpg /usr/share/desktop-base/kali-theme/login/background
 sed -i 's/#emblem-kali/\/home\/plantplants\/Pictures\/kirby.png/' /etc/lightdm/lightdm-gtk-greeter.conf
 sed -i 's/Kali-Light/Kali-Dark/' /etc/lightdm/lightdm-gtk-greeter.conf
@@ -37,4 +41,8 @@ virtualenv -p /usr/bin/python2 $toolbox/python2env
 echo 'export PATH="$PATH:$toolbox/python2env/bin"' >> /home/plantplants/.zshrc
 
 # apt install
-# apt install seclists
+apt install seclists
+
+# permissions
+chown plantplants:plantplants $toolbox
+chown plantplants:plantplants $toolbox/*
