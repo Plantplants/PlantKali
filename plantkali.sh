b9c45fad333dd944f70e69b6201d04122046d09b
Plant Plants <77038776+Plantplants@users.noreply.github.com>
2022-09-26 13:04:53 -0700

    Update plantkali.sh

--- plantkali.sh
+++ plantkali.sh
@@ -19,7 +19,7 @@ usermod -m -d /home/plantplants plantplants
 
 # Setting up toolbox
 mkdir $toolbox
-echo 'export PATH=$PATH:$toolbox' >> /home/plantplants/.zshrc
+echo 'export PATH="$PATH:$toolbox"' >> /home/plantplants/.zshrc
 
 # Backgrounds/logos
 apt install -y kali-wallpapers-legacy
@@ -35,14 +35,14 @@ sed -i 's/Kali-Light/Kali-Dark/' /etc/lightdm/lightdm-gtk-greeter.conf
 # pip install droopescan --target $toolbox/droopescan
 
 virtualenv -p /usr/bin/python2 $toolbox/python2env
-echo 'export PATH=$PATH:$toolbox/python2env/bin' >> /home/plantplants/.zshrc
+echo 'export PATH="$PATH:$toolbox/python2env/bin"' >> /home/plantplants/.zshrc
 
 ## go
 cd $userhome/Download
 wget https://go.dev/dl/go1.19.1.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.1.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-echo 'PATH=$PATH:/usr/local/go/bin' >> /home/plantplants/.zshrc
+export PATH="$PATH:/usr/local/go/bin"
+echo 'PATH="$PATH:/usr/local/go/bin"' >> /home/plantplants/.zshrc
 
 # apt install
 apt install seclists
